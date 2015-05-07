6b6d39caaf213115a2185c39634b667bfc305563
Bibudh Lahiri <bibudhlahiri@gmail.com>
2015-05-07 17:10:45 -0400

    Added code to create purchase values at random

--- rhadoop/client_merchant.R
+++ rhadoop/client_merchant.R
@@ -7,9 +7,20 @@ n_clients <- 100
 n_merchants <- 500
 k <- 10
 p <- 20
+high_vol_lower_limit <- 50
+high_vol_upper_limit <- 100
+
 #Assign each client to one of k clusters
 clients <- data.frame(id = 1:n_clients, cluster = sample(1:k, n_clients, replace = TRUE))
 #Pick the subset of p high-value merchants for each cluster and store it somewhere
 high_val_merchants <- lapply(as.list(1:k), function(x) sample(1:n_merchants, p))
+#Get the high-value merchants for each client
+out = to.dfs(keyval(rep(clients$id, each = p), unlist(high_val_merchants[clients$cluster])))
+client_high_val_merchant = as.data.frame(from.dfs(out))
+colnames(client_high_val_merchant) <- c("client_id", "high_val_merchant_id")
 #Assign high value to these merchants for these clients
-keyval(rep(clients$id, each = p), unlist(high_val_merchants[clients$cluster]))
+#client_high_val_merchant$purchase_vol <- sample(high_vol_lower_limit:high_vol_upper_limit, nrow(client_high_val_merchant), replace = TRUE)
+client_high_val_merchant = as.data.frame(keyval(client_high_val_merchant, sample(high_vol_lower_limit:high_vol_upper_limit, nrow(client_high_val_merchant), replace = TRUE)))
+colnames(client_high_val_merchant) <- c("client_id", "high_val_merchant_id", "purchase_vol")
+
+#Compute the cosine similarity between all pairs of clients/merchants(?)
