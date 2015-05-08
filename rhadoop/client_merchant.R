905a5f3561f60c086fed05975935ec580f57dfd6
Bibudh Lahiri <bibudhlahiri@gmail.com>
2015-05-08 17:01:57 -0400

    Added first two MR jobs

--- rhadoop/client_merchant.R
+++ rhadoop/client_merchant.R
@@ -1,5 +1,7 @@
 Sys.setenv(HADOOP_STREAMING = "/Users/blahiri/hadoop/hadoop-2.7.0/share/hadoop/tools/lib/hadoop-streaming-2.7.0.jar")
 Sys.setenv(HADOOP_CMD = "/Users/blahiri/hadoop/hadoop-2.7.0/bin/hadoop")
+#Sys.setenv(HADOOP_COMMON_LIB_NATIVE_DIR = "/Users/blahiri/hadoop/hadoop-2.7.0/lib/native")
+#Sys.setenv(HADOOP_OPTS = "-Djava.library.path = /Users/blahiri/hadoop/hadoop-2.7.0/lib")
 
 library(rmr2)
 
@@ -17,10 +19,38 @@ high_val_merchants <- lapply(as.list(1:k), function(x) sample(1:n_merchants, p))
 #Get the high-value merchants for each client
 out = to.dfs(keyval(rep(clients$id, each = p), unlist(high_val_merchants[clients$cluster])))
 client_high_val_merchant = as.data.frame(from.dfs(out))
-colnames(client_high_val_merchant) <- c("client_id", "high_val_merchant_id")
+colnames(client_high_val_merchant) <- c("client_id", "merchant_id")
 #Assign high value to these merchants for these clients
 #client_high_val_merchant$purchase_vol <- sample(high_vol_lower_limit:high_vol_upper_limit, nrow(client_high_val_merchant), replace = TRUE)
 client_high_val_merchant = as.data.frame(keyval(client_high_val_merchant, sample(high_vol_lower_limit:high_vol_upper_limit, nrow(client_high_val_merchant), replace = TRUE)))
-colnames(client_high_val_merchant) <- c("client_id", "high_val_merchant_id", "purchase_vol")
+colnames(client_high_val_merchant) <- c("client_id", "merchant_id", "purchase_vol")
+client_high_val_merchant = to.dfs(client_high_val_merchant)
 
 #Compute the cosine similarity between all pairs of clients/merchants(?)
+#First, compute the dot products between pairs of (column) vectors representing merchants
+map1 = function(., client_merchant) {
+          keyval(client_merchant$client_id, paste(client_merchant$merchant_id, "_", client_merchant$purchase_vol, sep = ""))
+       }
+mrjob1 = mapreduce(input = client_high_val_merchant, map = map1)
+client_others <- from.dfs(mrjob1)
+client_others = to.dfs(client_others)
+
+map2 = function(client, merchant_purchase) {
+   for (other1 in merchant_purchase)
+   {
+     for (other2 in merchant_purchase)
+     {
+       split1 = unlist(strsplit(other1, "_"))
+       merchant1 = split1[1]; purchase1 = split1[2]
+       split2 = unlist(strsplit(other2, "_"))
+       merchant2 = split2[1]; purchase2 = split2[2]
+       if (merchant1 != merchant2)
+       {
+         keyval(paste(merchant1, merchant2, "_", sep = ""), as.numeric(purchase1)*as.numeric(purchase2))
+       }
+     }
+   }
+}
+mrjob2 = mapreduce(input = mrjob1, map = map2)
+merchant_pair_products <- from.dfs(mrjob2)
+
