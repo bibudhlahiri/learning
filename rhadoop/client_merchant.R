acb19f869afbee94ce5c85d0150211c8963c6be4
Bibudh Lahiri <bibudhlahiri@gmail.com>
2015-05-14 15:33:17 -0400

    Added the second MR job

--- rhadoop/client_merchant.R
+++ rhadoop/client_merchant.R
@@ -4,6 +4,7 @@ Sys.setenv(HADOOP_CMD = "/Users/blahiri/hadoop/hadoop-2.7.0/bin/hadoop")
 #Sys.setenv(HADOOP_OPTS = "-Djava.library.path = /Users/blahiri/hadoop/hadoop-2.7.0/lib")
 
 library(rmr2)
+set.seed(1)
 
 rmr.options(backend = "hadoop")  
 hdfs.data.root = "client_merchant"
@@ -48,12 +49,11 @@ reduce1 = function(client, merchant_purchase) {
        merchant1 = split1[1]; purchase1 = split1[2]  
        split2 = unlist(strsplit(other2, "_"))
        merchant2 = split2[1]; purchase2 = split2[2]
-       if (merchant1 != merchant2)
+       if (merchant1 < merchant2)  # < ensures one pair of merchants appears exactly once for a client
        {
          str_key = paste(merchant1, "_", merchant2, sep = "")
          num_val = as.numeric(purchase1)*as.numeric(purchase2)
          merchant_pair_product <- paste(str_key, "_", num_val, sep = "")
-         cat(paste("str_key = ", str_key, ", num_val = ", num_val, ", merchant_pair_product = ", merchant_pair_product, "\n", sep = ""), file = stderr())
          clients_bag <- c(clients_bag, merchant_pair_product)
        }
      }
@@ -61,5 +61,37 @@ reduce1 = function(client, merchant_purchase) {
    return(keyval(client, clients_bag))
 }
 system("rm -r /Users/blahiri/learning/rhadoop/merchant_pair_products")
-mrjob1 = mapreduce(input = client_high_val_merchant, output = "/Users/blahiri/learning/rhadoop/merchant_pair_products/", output.format = "csv", map = map1, reduce = reduce1)
-results <- from.dfs(input = "/Users/blahiri/learning/rhadoop/merchant_pair_products/")
+mrjob1 = mapreduce(input = client_high_val_merchant, output = "/Users/blahiri/learning/rhadoop/merchant_pair_products/", 
+                   output.format = make.output.format(format = "csv", mode = "text", sep = ","), 
+                   map = map1, reduce = reduce1)
+#results <- from.dfs(input = "/Users/blahiri/learning/rhadoop/merchant_pair_products/")
+
+asa.csv.input.format = make.input.format(format='csv', mode='text', streaming.format = NULL, sep=',',
+										 col.names = c('client', 'merchant_pair_product'),
+										 stringsAsFactors=F)
+library(stringr)
+map2 = function(., merchant_pair_products) {
+          #Input value to mapper function is a data frame
+          df <- as.data.frame(str_split_fixed(merchant_pair_products$merchant_pair_product, "_", 3))
+          colnames(df) <- c("merchant1", "merchant2", "product")
+          keyval(paste(df$merchant1, "_", df$merchant2, sep = ""), df$product)
+       }
+reduce2 = function(merchant_pair, product) {
+  #Since product was originally a vector of factor variables, had to cast to characters before converting to numeric.
+  product <- as.numeric(as.character(product))
+  n <- length(product)
+  dot_product <- 0
+  for (i in 1:n)
+  {
+    dot_product <- dot_product + product[i]
+    cat(paste("merchant_pair = ", merchant_pair, ", product[i] = ", product[i], ", dot_product = ", dot_product, "\n", sep = ""), file = stderr())
+  }
+  #dot_product = sum(as.numeric(product))
+  cat(paste("merchant_pair = ", merchant_pair, ", dot_product = ", dot_product, "\n", sep = ""), file = stderr())
+  return(keyval(merchant_pair, dot_product))
+}
+system("rm -r /Users/blahiri/learning/rhadoop/merchant_pair_dot_product")
+mrjob2 = mapreduce(input = "/Users/blahiri/learning/rhadoop/merchant_pair_products/",  
+                   input.format = asa.csv.input.format,
+                   output = "/Users/blahiri/learning/rhadoop/merchant_pair_dot_product/", 
+                   output.format = "csv", map = map2, reduce = reduce2)
