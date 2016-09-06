9d98e201f3c690b761b8d151556049db1bd2e005
Bibudh Lahiri <blahiri@impetus.co.in>
2016-09-06 15:01:26 +0530

    Tuning is at a reasonable point with three classes

--- data_gen_allstate.R
+++ data_gen_allstate.R
@@ -10,7 +10,7 @@ gen_driving_data <- function()
   n_high_risk <- 0.33*N
   high_risk <- data.table(time_of_day = sample(c("night", "day"), n_high_risk, replace = TRUE, prob = c(0.95, 0.05)))
   high_risk[, raining := sample(c("true", "false"), n_high_risk, replace = TRUE, prob = c(0.95, 0.05))]
-  high_risk[, accident_prone_area := sample(c("true", "false"), n_high_risk, replace = TRUE, prob = c(0.7, 0.3))]
+  high_risk[, accident_prone_area := sample(c("true", "false"), n_high_risk, replace = TRUE, prob = c(0.95, 0.05))]
   high_risk[, g_force := c(runif(n_high_risk/2, 0.4, 5), runif(n_high_risk/2, -5, -0.4))]
   high_risk[, n_prev_instances := rbinom(n_high_risk, 4, 0.6)]
   high_risk[, risky := sample(c("high", "medium", "low"), n_high_risk, replace = TRUE, prob = c(0.9, 0.05, 0.05))]
@@ -18,13 +18,13 @@ gen_driving_data <- function()
   n_medium_risk <- 0.33*N
   medium_risk <- data.table(time_of_day = sample(c("night", "day"), n_medium_risk, replace = TRUE, prob = c(0.8, 0.2)))
   medium_risk[, raining := sample(c("true", "false"), n_medium_risk, replace = TRUE, prob = c(0.8, 0.2))]
-  medium_risk[, accident_prone_area := sample(c("true", "false"), n_medium_risk, replace = TRUE, prob = c(0.6, 0.4))]
+  medium_risk[, accident_prone_area := sample(c("true", "false"), n_medium_risk, replace = TRUE, prob = c(0.8, 0.2))]
   medium_risk[, g_force := c(runif(n_medium_risk/2, 0.2, 0.4), runif(n_medium_risk/2, -0.4, -0.2))]
   medium_risk[, n_prev_instances := rbinom(n_medium_risk, 3, 0.6)]
   medium_risk[, risky := sample(c("high", "medium", "low"), n_medium_risk, replace = TRUE, prob = c(0.05, 0.9, 0.05))]
   
   n_low_risk <- N - n_high_risk - n_medium_risk 
-  low_risk <- data.table(time_of_day = sample(c("night", "day"), n_low_risk, replace = TRUE, prob = c(0.5, 0.5)))
+  low_risk <- data.table(time_of_day = sample(c("night", "day"), n_low_risk, replace = TRUE, prob = c(0.2, 0.8)))
   low_risk[, raining := sample(c("true", "false"), n_low_risk, replace = TRUE, prob = c(0.2, 0.8))]
   low_risk[, accident_prone_area := sample(c("true", "false"), n_low_risk, replace = TRUE, prob = c(0.2, 0.8))]
   low_risk[, g_force := runif(n_low_risk, -0.2, 0.2)]
@@ -35,8 +35,8 @@ gen_driving_data <- function()
   id_start <- 217654
   driving_data[, id := sample(id_start:(id_start+N-1), replace = FALSE)]
   
-  #filename <- "C:\\Users\\blahiri\\AllState\\driving_data.csv"
-  #write.table(driving_data, filename, sep = ",", row.names = FALSE, col.names = TRUE, quote = FALSE)
+  filename <- "C:\\Users\\blahiri\\AllState\\driving_data.csv"
+  write.table(driving_data, filename, sep = ",", row.names = FALSE, col.names = TRUE, quote = FALSE)
   driving_data
 }
 
