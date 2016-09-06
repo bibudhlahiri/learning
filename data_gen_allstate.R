5f16aeb4d1f0d2d5ff820b7575bae8603a5a0696
Bibudh Lahiri <blahiri@impetus.co.in>
2016-09-06 14:56:15 +0530

    First commit after splitting response variable into three classes

--- data_gen_allstate.R
+++ data_gen_allstate.R
@@ -7,28 +7,36 @@ gen_driving_data <- function()
 {
   N <- 100000
     
-  n_risky <- 0.5*N
-  risky <- data.table(time_of_day = sample(c("night", "day"), n_risky, replace = TRUE, prob = c(0.95, 0.05)))
-  risky[, raining := sample(c("true", "false"), n_risky, replace = TRUE, prob = c(0.95, 0.05))]
-  risky[, accident_prone_area := sample(c("true", "false"), n_risky, replace = TRUE, prob = c(0.7, 0.3))]
-  risky[, g_force := c(rnorm(n_risky/2, 0.185, 0.1), rnorm(n_risky/2, -0.185, 0.1))]
-  risky[, n_prev_instances := rbinom(n_risky, 4, 0.6)]
-  risky[, risky := sample(c("true", "false"), n_risky, replace = TRUE, prob = c(0.9, 0.1))]
+  n_high_risk <- 0.33*N
+  high_risk <- data.table(time_of_day = sample(c("night", "day"), n_high_risk, replace = TRUE, prob = c(0.95, 0.05)))
+  high_risk[, raining := sample(c("true", "false"), n_high_risk, replace = TRUE, prob = c(0.95, 0.05))]
+  high_risk[, accident_prone_area := sample(c("true", "false"), n_high_risk, replace = TRUE, prob = c(0.7, 0.3))]
+  high_risk[, g_force := c(runif(n_high_risk/2, 0.4, 5), runif(n_high_risk/2, -5, -0.4))]
+  high_risk[, n_prev_instances := rbinom(n_high_risk, 4, 0.6)]
+  high_risk[, risky := sample(c("high", "medium", "low"), n_high_risk, replace = TRUE, prob = c(0.9, 0.05, 0.05))]
   
-  n_non_risky <- N - n_risky
-  non_risky <- data.table(time_of_day = sample(c("night", "day"), n_non_risky, replace = TRUE, prob = c(0.5, 0.5)))
-  non_risky[, raining := sample(c("true", "false"), n_non_risky, replace = TRUE, prob = c(0.5, 0.5))]
-  non_risky[, accident_prone_area := sample(c("true", "false"), n_non_risky, replace = TRUE, prob = c(0.5, 0.5))]
-  non_risky[, g_force := c(rnorm(n_non_risky/2, 0.08, 0.04), rnorm(n_non_risky/2, -0.08, 0.04))]
-  non_risky[, n_prev_instances := rbinom(n_non_risky, 2, 0.6)]
-  non_risky[, risky := sample(c("true", "false"), n_non_risky, replace = TRUE, prob = c(0.1, 0.9))]
+  n_medium_risk <- 0.33*N
+  medium_risk <- data.table(time_of_day = sample(c("night", "day"), n_medium_risk, replace = TRUE, prob = c(0.8, 0.2)))
+  medium_risk[, raining := sample(c("true", "false"), n_medium_risk, replace = TRUE, prob = c(0.8, 0.2))]
+  medium_risk[, accident_prone_area := sample(c("true", "false"), n_medium_risk, replace = TRUE, prob = c(0.6, 0.4))]
+  medium_risk[, g_force := c(runif(n_medium_risk/2, 0.2, 0.4), runif(n_medium_risk/2, -0.4, -0.2))]
+  medium_risk[, n_prev_instances := rbinom(n_medium_risk, 3, 0.6)]
+  medium_risk[, risky := sample(c("high", "medium", "low"), n_medium_risk, replace = TRUE, prob = c(0.05, 0.9, 0.05))]
   
-  driving_data <- rbindlist(list(risky, non_risky))
+  n_low_risk <- N - n_high_risk - n_medium_risk 
+  low_risk <- data.table(time_of_day = sample(c("night", "day"), n_low_risk, replace = TRUE, prob = c(0.5, 0.5)))
+  low_risk[, raining := sample(c("true", "false"), n_low_risk, replace = TRUE, prob = c(0.2, 0.8))]
+  low_risk[, accident_prone_area := sample(c("true", "false"), n_low_risk, replace = TRUE, prob = c(0.2, 0.8))]
+  low_risk[, g_force := runif(n_low_risk, -0.2, 0.2)]
+  low_risk[, n_prev_instances := rbinom(n_low_risk, 2, 0.6)]
+  low_risk[, risky := sample(c("high", "medium", "low"), n_low_risk, replace = TRUE, prob = c(0.05, 0.05, 0.9))]
+  
+  driving_data <- rbindlist(list(high_risk, medium_risk, low_risk))
   id_start <- 217654
   driving_data[, id := sample(id_start:(id_start+N-1), replace = FALSE)]
-  #print(head(driving_data))
-  filename <- "C:\\Users\\blahiri\\AllState\\driving_data.csv"
-  write.table(driving_data, filename, sep = ",", row.names = FALSE, col.names = TRUE, quote = FALSE)
+  
+  #filename <- "C:\\Users\\blahiri\\AllState\\driving_data.csv"
+  #write.table(driving_data, filename, sep = ",", row.names = FALSE, col.names = TRUE, quote = FALSE)
   driving_data
 }
 
@@ -63,9 +71,7 @@ classify_driving_data_dtree <- function()
   #Measure overall accuracy
   setkey(test_data, risky, predicted_risky)
   accuracy <- nrow(test_data[(risky == predicted_risky),])/nrow(test_data)
-  recall <- prec_recall[2,2]/sum(prec_recall[2,])
-  precision <- prec_recall[2,2]/sum(prec_recall[,2])
-  cat(paste("Overall accuracy = ", accuracy, ", recall = ", recall, ", precision = ", precision, "\n\n", sep = "")) 
+  cat(paste("Overall accuracy = ", accuracy, "\n\n", sep = "")) 
   
   print(bestmod)
   print(bestmod$variable.importance/sum(bestmod$variable.importance))
