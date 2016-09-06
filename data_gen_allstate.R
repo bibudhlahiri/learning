88eb35bc2d8b1de50b9ec89cdec2f81047f5a875
Bibudh Lahiri <blahiri@impetus.co.in>
2016-09-06 12:23:05 +0530

    Found the optimal parameter combinations

--- data_gen_allstate.R
+++ data_gen_allstate.R
@@ -1,30 +1,29 @@
 library(data.table)
 library(e1071)
 library(rpart)
+set.seed(1)
 
 gen_driving_data <- function()
 {
   N <- 100000
-  g_force <- rnorm(N, 0.08, 0.12)
-  driving_data <- data.table(g_force = g_force)
-  setkey(driving_data, g_force)
+    
+  n_risky <- 0.5*N
+  risky <- data.table(time_of_day = sample(c("night", "day"), n_risky, replace = TRUE, prob = c(0.95, 0.05)))
+  risky[, raining := sample(c("true", "false"), n_risky, replace = TRUE, prob = c(0.95, 0.05))]
+  risky[, accident_prone_area := sample(c("true", "false"), n_risky, replace = TRUE, prob = c(0.95, 0.05))]
+  risky[, g_force := c(rnorm(n_risky/2, 0.185, 0.1), rnorm(n_risky/2, -0.185, 0.1))]
+  risky[, risky := sample(c("true", "false"), n_risky, replace = TRUE, prob = c(0.9, 0.1))]
   
-  risky <- driving_data[abs(g_force) >= 0.2]
-  non_risky <- driving_data[abs(g_force) < 0.2]
-  cat(paste("nrow(risky) = ", nrow(risky), ", nrow(non_risky) = ", nrow(non_risky),
-            ", total = ", sum(nrow(risky) + nrow(non_risky)), "\n", sep = ""))
-  
-  risky[, time_of_day := sample(c("night", "day"), nrow(risky), replace = TRUE, prob = c(0.95, 0.05))]
-  risky[, raining := sample(c("true", "false"), nrow(risky), replace = TRUE, prob = c(0.95, 0.05))]
-  risky[, accident_prone_area := sample(c("true", "false"), nrow(risky), replace = TRUE, prob = c(0.95, 0.05))]
-  risky[, risky := "true"]
-  
-  non_risky[, time_of_day := sample(c("night", "day"), nrow(non_risky), replace = TRUE, prob = c(0.5, 0.5))]
-  non_risky[, raining := sample(c("true", "false"), nrow(non_risky), replace = TRUE, prob = c(0.5, 0.5))]
-  non_risky[, accident_prone_area := sample(c("true", "false"), nrow(non_risky), replace = TRUE, prob = c(0.5, 0.5))]
-  non_risky[, risky := "false"]
+  n_non_risky <- N - n_risky
+  non_risky <- data.table(time_of_day = sample(c("night", "day"), n_non_risky, replace = TRUE, prob = c(0.5, 0.5)))
+  non_risky[, raining := sample(c("true", "false"), n_non_risky, replace = TRUE, prob = c(0.5, 0.5))]
+  non_risky[, accident_prone_area := sample(c("true", "false"), n_non_risky, replace = TRUE, prob = c(0.5, 0.5))]
+  non_risky[, g_force := c(rnorm(n_non_risky/2, 0.08, 0.04), rnorm(n_non_risky/2, -0.08, 0.04))]
+  non_risky[, risky := sample(c("true", "false"), n_non_risky, replace = TRUE, prob = c(0.1, 0.9))]
   
   driving_data <- rbindlist(list(risky, non_risky))
+  print(head(driving_data))
+  driving_data
 }
 
 classify_driving_data_dtree <- function()
@@ -37,7 +36,6 @@ classify_driving_data_dtree <- function()
   cat(paste("Size of training data = ", length(train), ", size of test data = ", (nrow(driving_data) - length(train)), "\n", sep = ""))
   
   training_data <- driving_data[train, ]
-  #training_data <- create_bs_by_over_and_undersampling(training_data)
   test_data <- driving_data[test, ]
   
   print(table(training_data$risky)) 
@@ -45,13 +43,11 @@ classify_driving_data_dtree <- function()
   
   #cols <- c("session_id", "flow_id")            
   
-  tune.out <- tune.rpart(risky ~ ., 
-                         data = training_data,
-                         #data = training_data[, .SD, .SDcols = -cols], 
-                         minsplit = c(5, 10, 15, 20), maxdepth = seq(5, 30, 5))
-  print(summary(tune.out))
-  
-  bestmod <- tune.out$best.model
+  bestmod <- rpart(risky ~ ., 
+                   data = training_data,
+                   #data = training_data[, .SD, .SDcols = -cols]
+                   control = rpart.control(maxdepth = 10)
+                   )
   test_data[, predicted_risky := as.character(predict(bestmod, newdata = test_data, type = "class"))]
   
   prec_recall <- table(test_data[, risky], test_data[, predicted_risky], dnn = list('actual', 'predicted'))
@@ -62,7 +58,7 @@ classify_driving_data_dtree <- function()
   accuracy <- nrow(test_data[(risky == predicted_risky),])/nrow(test_data)
   recall <- prec_recall[2,2]/sum(prec_recall[2,])
   precision <- prec_recall[2,2]/sum(prec_recall[,2])
-  cat(paste("Overall accuracy = ", accuracy, ", recall = ", recall, ", precision = ", precision, "\n\n", sep = "")) #0.9273255
+  cat(paste("Overall accuracy = ", accuracy, ", recall = ", recall, ", precision = ", precision, "\n\n", sep = "")) 
   
   print(bestmod)
   print(bestmod$variable.importance/sum(bestmod$variable.importance))
