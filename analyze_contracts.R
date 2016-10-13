21a84fa87b972dca84a824917632c468dfe7ada4
Bibudh Lahiri <blahiri@impetus.co.in>
2016-10-12 19:07:21 -0500

    Added methods check_if_column_name and analyze_contract_expiry

--- analyze_contracts.R
+++ analyze_contracts.R
@@ -24,7 +24,33 @@ load_comet_data <- function()
 								   "numeric", "numeric", "numeric", "numeric", "numeric",
 								   "character", "character", "character", "character"),
                     data.table = TRUE)
+  lines <- readLines("C:\\Users\\blahiri\\Verizon\\COMET_DATA_2016\\Schema.txt")
+  from_schema_file <- unlist(strsplit(lines, " "))
+  is_column_name <- unlist(lapply(from_schema_file, check_if_column_name))
+  column_names <- from_schema_file[which(is_column_name == TRUE)]
+  setnames(comet_data, names(comet_data), column_names)
+  comet_data
 }
+
+check_if_column_name <- function(input)
+{
+  if ((input == "") || (substr(input, 1, nchar("VARCHAR")) == "VARCHAR") || (substr(input, 1, nchar("SMALLINT")) == "SMALLINT") ||
+      (substr(input, 1, nchar("INTDATE")) == "INTDATE") || (substr(input, 1, nchar("DECIMAL")) == "DECIMAL") || 
+	  (substr(input, 1, nchar("INTEGER")) == "INTEGER") || (substr(input, 1, nchar("CHAR")) == "CHAR"))
+	 return(FALSE)
+  return(TRUE)
+} 
+
+analyze_contract_expiry <- function(comet_data)
+{
+  minus_90_date <- Sys.Date() - 90
+  plus_90_date <- Sys.Date() + 90
+  comet_data[, TRACKER_DT := gsub("/", "-", comet_data$TRACKER_DT)]
+  setkey(comet_data, TRACKER_DT)
+  for_today <- comet_data[((TRACKER_DT >= as.character(minus_90_date)) & (TRACKER_DT <= as.character(plus_90_date))),]
+  for_today[, renewed := (RENEW_DATE != "")]
+}
+
 
 
 
