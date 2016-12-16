ee9f1bcc5e55c191a14543c3b95df5ab54bda088
Bibudh Lahiri <blahiri@impetus.co.in>
2016-12-16 16:43:06 +0530

    Added methods get_image_names and fill_grid_for_given_image

--- compose_labels.r
+++ compose_labels.r
@@ -1,6 +1,17 @@
 library(rjson)
 library(stringdist)
 
+get_image_names <- function(cnn_results)
+{
+  num_images <- length(cnn_results[["results"]])
+  image_names <- c()
+  for (i in 1:num_images)
+  {
+    image_names <- c(image_names, cnn_results[["results"]][[i]][["img_name"]])
+  }
+  image_names
+}
+
 fill_grid_with_labels <- function(cnn_results, cutoff = 15, serial_no = 1)
 {
    cat(paste("Image file is ", cnn_results[["results"]][[serial_no]][["img_name"]], "\n", sep = ""))
@@ -29,9 +40,9 @@ find_best_match <- function(cnn_results, i, j, cutoff = 15, serial_no = 1, cell_
 						 
   best_score <- Inf
   best_match <- "*" 
-  cat(paste("i = ", i, ", j = ", j, "\n", sep = ""))
-  cat("cell_boundaries\n")
-  print(cell_boundaries)
+  #cat(paste("i = ", i, ", j = ", j, "\n", sep = ""))
+  #cat("cell_boundaries\n")
+  #print(cell_boundaries)
   #cat(paste("cutoff = ", cutoff, "\n", sep = ""))
   for (k in 1:cutoff)
   {
@@ -51,8 +62,8 @@ find_best_match <- function(cnn_results, i, j, cutoff = 15, serial_no = 1, cell_
 	  boundaries_for_best_match <- box_boundaries
 	}
   }
-  cat("boundaries_for_best_match\n")
-  print(round(boundaries_for_best_match))
+  #cat("boundaries_for_best_match\n")
+  #print(round(boundaries_for_best_match))
   best_match
 }
 
@@ -76,11 +87,10 @@ extract_n_grams <- function(m_grid, n = 3)
   n_grams
 }
 
-remove_junk_chars <- function(m_grid)
+remove_junk_chars <- function(m_grid, dictionary)
 {
   #Scan the n-grams (n = 3, 4 and 5) from rows. Have a dictionary. Do fuzzy lookup.
   candidates <- c(extract_n_grams(m_grid, 3), extract_n_grams(m_grid, 4), extract_n_grams(m_grid, 5))
-  dictionary <- c("108", "NOTE", "1VN-998")
   cand_dic <- expand.grid(candidate = candidates, dict_entry = dictionary)
   #Note: string distance is case-sensitive, so converting to uppercase for now
   cand_dic$candidate <- toupper(cand_dic$candidate)
@@ -88,14 +98,33 @@ remove_junk_chars <- function(m_grid)
                                                                    as.character(row["dict_entry"]), method = "dl"))
   #What fraction of characters in the dictionary string match the candidate in the right order?
   cand_dic$dict_entry_length <- apply(cand_dic, 1, function(row) nchar(as.character(row["dict_entry"])))
-  cand_dic$fuzzy_match_score <- cand_dic$distance/cand_dic$dict_entry_length
-  cand_dic <- cand_dic[with(cand_dic, order(fuzzy_match_score)),]
+  cand_dic$fuzzy_match_score <- 1 - cand_dic$distance/cand_dic$dict_entry_length
+  cand_dic <- cand_dic[with(cand_dic, order(-fuzzy_match_score)),]
+}
+
+fill_grid_for_given_image <- function(image_names, image_name)
+{
+  which(image_names == image_name)
 }
 
 #source("C:\\Users\\blahiri\\Chevron\\compose_labels.r")
 cnn_results <- fromJSON(file = "C:\\Users\\blahiri\\Chevron\\vis\\data\\results.json")
-m_grid <- fill_grid_with_labels(cnn_results, cutoff = 15, serial_no = 1)
-cand_dic <- remove_junk_chars(m_grid)
+image_names <- get_image_names(cnn_results)
+#dictionary <- c("108", "NOTE", "1VN-998") #For serial_no = 1
+#dictionary <- c("1VN-998", "PCP-14", "FSV 8425") #For serial_no = 2
+#dictionary <- c("1VN-999") #For serial_no = 3
+#dictionary <- c("14", "15", "16", "17") #For serial_no = 4
+#dictionary <- c("14", "15", "16", "17") #For serial_no = 5
+#dictionary <- c("PAHH6305B") #For serial_no = 6
+#dictionary <- c("S", "PAHH8426A1", "IP-PCP-14", "BYPASS") #For serial_no = 7
+#dictionary <- c("1VN-998", "FSV 8427") #For serial_no = 8
+#dictionary <- c("FOR DESIGN", "DESIGN", "REVIEW", "RIPTION", "SIONS") #For serial_no = 9
+#dictionary <- c("1VB-71", "1-FL-C2A-2876", "VENDOR") #For serial_no = 10
+#dictionary <- c("P-C2A-1837", "VB-71", "A2A", "C2A") #For serial_no = 12
+#dictionary <- c("H") #For serial_no = 13
+inp_serial_no <- fill_grid_for_given_image(image_names, "211.jpg")
+m_grid <- fill_grid_with_labels(cnn_results, cutoff = 15, serial_no = inp_serial_no)
+cand_dic <- remove_junk_chars(m_grid, dictionary)
 print(head(cand_dic, 20))
 
 
