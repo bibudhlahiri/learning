7e293b825eb2413136b20677aede66804c9044bc
Bibudh Lahiri <blahiri@impetus.co.in>
2016-12-22 11:17:56 +0530

    Added methods rowwise_fuzzy_score, extract_tokens_horizontally and extract_tokens_vertically

--- compose_labels_from_tess_op.r
+++ compose_labels_from_tess_op.r
@@ -33,10 +33,7 @@ fill_grid_with_labels <- function(tess_results, image_name, flipped)
 	   m_grid[i, j] <- find_best_match(data_for_this_image, i, j, cell_width, cell_height)
 	 }
    } 
-   print(m_grid)
-   #cat("scores_for_grid_cells\n")
-   #print(scores_for_grid_cells)
-   m_grid <- remove_junk_chars(m_grid, data_for_this_image, scores_for_grid_cells, n_rows, n_columns, image_name)   
+   m_grid <- remove_junk_chars(m_grid, data_for_this_image, scores_for_grid_cells, n_rows, n_columns)   
    print(m_grid)
    return(m_grid)
 }
@@ -83,40 +80,67 @@ find_best_match <- function(data_for_this_image, i, j, cell_width, cell_height)
 #matched character, if it has been allocated to multiple cells, we will keep
 #it only in that cell where its score (based on differences of coordinates) is highest, 
 #and remove it from everywhere else.
-remove_junk_chars <- function(m_grid, data_for_this_image, scores_for_grid_cells, n_rows, n_columns, image_name)   
+remove_junk_chars <- function(m_grid, data_for_this_image, scores_for_grid_cells, n_rows, n_columns)   
 {
   #Find the lowest (best) score for each char_id
   best_scores_for_chars <- aggregate(scores_for_grid_cells$score, by = list(scores_for_grid_cells$id_of_char_placed), FUN = min)
   colnames(best_scores_for_chars) <- c("id_of_char_placed", "score")
   best_scores_for_chars <- merge(best_scores_for_chars, scores_for_grid_cells)
-  #cat("best_scores_for_chars\n")
-  #print(best_scores_for_chars)
-  cat(paste("n_rows = ", n_rows, ", n_columns = ", n_columns, "\n", sep = ""))
+  
   for (i in 1:(n_rows*n_columns))
   {
-        #Go through each cell of m_grid (using scores_for_grid_cells), and find out the best position for the char placed in the cell 
-        #from best_scores_for_chars. If the position of the current cell does not match the best position for the placed character,
-        #fill the cell with a special character like "@".
-        record_for_best_position <- subset(best_scores_for_chars, (best_scores_for_chars$id_of_char_placed == scores_for_grid_cells[i, "id_of_char_placed"]))
-        if (image_name == "1010.jpg")
-        {
-          cat(paste("i = ", i, ", scores_for_grid_cells[i, id_of_char_placed] = ", scores_for_grid_cells[i, "id_of_char_placed"], "\n", sep = ""))
-          if (scores_for_grid_cells[i, "id_of_char_placed"] == 12)
-          {
-            cat("record_for_best_position\n")
-            print(record_for_best_position)
-          }
-        }
-        if (!((record_for_best_position[1, "row_number"] == scores_for_grid_cells[i, "row_number"])
+    #Go through each cell of m_grid (using scores_for_grid_cells), and find out the best position for the char placed in the cell 
+    #from best_scores_for_chars. If the position of the current cell does not match the best position for the placed character,
+    #fill the cell with a special character like "@".
+    record_for_best_position <- subset(best_scores_for_chars, (best_scores_for_chars$id_of_char_placed == scores_for_grid_cells[i, "id_of_char_placed"]))
+    if (!((record_for_best_position[1, "row_number"] == scores_for_grid_cells[i, "row_number"])
              && (record_for_best_position[1, "col_number"] == scores_for_grid_cells[i, "col_number"])))
-        {
-          #cat("Putting @\n")
-          m_grid[scores_for_grid_cells[i, "row_number"], scores_for_grid_cells[i, "col_number"]] <- "@"
-        }
+    {
+      m_grid[scores_for_grid_cells[i, "row_number"], scores_for_grid_cells[i, "col_number"]] <- "@"
+    }
   }
   m_grid
 }
 
+
+#Extract tokens and not n-grams of fixed length. A token is a seuqence of non-empty characters in the grid. 
+#Can be of any length.
+extract_tokens_horizontally <- function(m_grid)
+{
+  tokens <- c()
+  n_rows <- nrow(m_grid)
+  n_columns <- ncol(m_grid)
+  
+  for (i in 1:n_rows)
+  {
+    string <- paste(m_grid[i, 1:n_columns], collapse = "")
+	raw_tokens <- unlist(strsplit(string, "@"))
+	tokens <- c(tokens, raw_tokens[raw_tokens != ""])
+  }
+  tokens
+}
+
+extract_tokens_vertically <- function(m_grid)
+{
+  tokens <- c()
+  n_rows <- nrow(m_grid)
+  n_columns <- ncol(m_grid)
+  
+  for (i in 1:n_columns)
+  {
+    string <- paste(m_grid[n_rows:1, i], collapse = "")
+	raw_tokens <- unlist(strsplit(string, "@"))
+	tokens <- c(tokens, raw_tokens[raw_tokens != ""])
+  }
+  tokens
+}
+
+#TBD: Some numeric tokens are part of larger tokens, e.g., 84251 is extracted from 37.jpg which is actually 
+#a part of "PAHH 8425A1". In such cases, we will check if the number-only string is part of some bigger string 
+#in the global dictionary. If it is, we will see if the first part of the string from the dictionary (e.g., "PAHH")
+#also appears as a token (at least partly). If it does, we will combine the alphabetic and numeric parts, e.g. PAHH
+#and 84251, and keep that as a candidate string and deleting the tokens like "PAHH" and "84251" from the set of candidates.
+#This needs to be done since strings like "PAHH 8425A1" are appearing over multiple rows in the grid. 
 
 #Extracts all distinct horizontal n-grams from a matrix 
 extract_n_grams_horizontally <- function(m_grid, n = 3)
@@ -159,6 +183,17 @@ extract_n_grams_vertically <- function(m_grid, n = 3)
 }
 
 
+rowwise_fuzzy_score <- function(candidate, dict_entry)
+{
+  #If the dictionary string has only 3 characters, it should match on all. Otherwise, set score to 0.
+  #if ((nchar(dict_entry) == 3) &&  (!(dict_entry == candidate)))
+  #{
+  #  return(0)
+  #}
+  distance <- stringdist(candidate, dict_entry, method = "dl")
+  dict_entry_length <- nchar(dict_entry)
+  fuzzy_match_score <- 1 - distance/dict_entry_length
+}
 
 calc_fuzzy_score <- function(m_grid, dictionary, flipped)
 {
@@ -166,20 +201,24 @@ calc_fuzzy_score <- function(m_grid, dictionary, flipped)
   #Scan the n-grams (n = 3, 4 and 5) from rows/columns, depending on flipped. Have a dictionary. Do fuzzy lookup.
   if (!flipped)
   {
-    candidates <- c(extract_n_grams_horizontally(m_grid, 3), extract_n_grams_horizontally(m_grid, 4), extract_n_grams_horizontally(m_grid, 5))
+    #candidates <- c(extract_n_grams_horizontally(m_grid, 3), extract_n_grams_horizontally(m_grid, 4), extract_n_grams_horizontally(m_grid, 5))
+	candidates <- extract_tokens_horizontally(m_grid)
   }
   else
   {
-    candidates <- c(extract_n_grams_vertically(m_grid, 3), extract_n_grams_vertically(m_grid, 4), extract_n_grams_vertically(m_grid, 5))
+    #candidates <- c(extract_n_grams_vertically(m_grid, 3), extract_n_grams_vertically(m_grid, 4), extract_n_grams_vertically(m_grid, 5))
+	candidates <- extract_tokens_vertically(m_grid)
   }
   cand_dic <- expand.grid(candidate = candidates, dict_entry = dictionary)
   #Note: string distance is case-sensitive, so converting to uppercase for now
   cand_dic$candidate <- toupper(cand_dic$candidate)
-  cand_dic$distance <- apply(cand_dic, 1, function(row) stringdist(as.character(row["candidate"]), 
-                                                                   as.character(row["dict_entry"]), method = "dl"))
+  #cand_dic$distance <- apply(cand_dic, 1, function(row) stringdist(as.character(row["candidate"]), 
+  #                                                                 as.character(row["dict_entry"]), method = "dl"))
   #What fraction of characters in the dictionary string match the candidate in the right order?
-  cand_dic$dict_entry_length <- apply(cand_dic, 1, function(row) nchar(as.character(row["dict_entry"])))
-  cand_dic$fuzzy_match_score <- 1 - cand_dic$distance/cand_dic$dict_entry_length
+  #cand_dic$dict_entry_length <- apply(cand_dic, 1, function(row) nchar(as.character(row["dict_entry"])))
+  #cand_dic$fuzzy_match_score <- 1 - cand_dic$distance/cand_dic$dict_entry_length
+  cand_dic$fuzzy_match_score <- apply(cand_dic, 1, function(row) rowwise_fuzzy_score(as.character(row["candidate"]), 
+                                                                                     as.character(row["dict_entry"])))
   cand_dic <- cand_dic[with(cand_dic, order(-fuzzy_match_score)),]
 }
 
@@ -277,7 +316,6 @@ measure_recall_precision <- function(global_dictionary, tess_results, threshold 
 	{
 	  cand_dic <- cand_dic_vertical
 	}
-	
 	flagged <- subset(cand_dic, (fuzzy_match_score >= threshold))
 	reported_matches <- unique(flagged$dict_entry)
 	
@@ -315,14 +353,14 @@ colnames(tess_results) <- c("FileName", "LHS_X", "LHS_Y", "RHS_X", "RHS_Y", "CHA
 #Creating this as a named list so that lookup can be performed by name,
 #and not by index. That way, the order of filenames in output.csv 
 #does not matter.
-global_dictionary <- list("35.jpg" = c("6-P-C5-7513", "(MINIMUM FLOW)", "842"),
+global_dictionary <- list("35.jpg" = c("6-P-C5-7513", "(MINIMUM FLOW)"),
                           "37.jpg" = c("IP-PCP-14", "PAHH 8425A1"), 
 						  "86.jpg" = c("6VB-75G", "C5-1826"), 
 						  "92.jpg" = c("2-BD-C5-2587"),
                           "93.jpg" = c("CONTRACTOR"), 
 						  "99.jpg" = c("1-P-C2A-1838", "A2A"),  
 						  "112.jpg" = c("SP101"), 
-						  "116.jpg" = c("XA8427A1", "PALL8427A", "IP-PCP-14", "C5-1827"), 
+						  "116.jpg" = c("XA8427A1", "PALL8427A", "IP-PCP-14", "P-C5-1827"), 
 						  "126.jpg" = c("6VB-75G"), 
 						  "127.jpg" = c("FSV 8427A", "FO 8427", "6VC-460", "1VN-998", "VB-75M"), 
 						  "128.jpg" = c("FV8427", "4FO", "6-P-C5-1813"), 
@@ -334,7 +372,9 @@ global_dictionary <- list("35.jpg" = c("6-P-C5-7513", "(MINIMUM FLOW)", "842"),
 						  "910.jpg" = c("10VB-75GCSO", "1VN-998S", "C2A", "HPV"), 
 						  "1010.jpg" = c("1FL-C2A-2876", "1VB-71", "C2A", "VENDOR") 
 						  )
-measure_recall_precision(global_dictionary, tess_results, threshold = 0.4)
+sink("C:\\Users\\blahiri\\Chevron\\log.txt")
+measure_recall_precision(global_dictionary, tess_results, threshold = 0.35)
+sink()
 #Before implementing flipped characters, 
 #With threshold = 0.35, avg recall = 0.9, avg precision = 0.68
 #With threshold = 0.39, avg recall = 0.85, avg precision = 0.7
@@ -343,6 +383,16 @@ measure_recall_precision(global_dictionary, tess_results, threshold = 0.4)
 #After implementing flipped characters, 
 #With threshold = 0.4, avg recall = 0.9083, avg precision = 0.373
 #With threshold = 0.5, avg recall = 0.621296296296296, avg precision = 0.37781774325892
+
+#After implementing remove_junk_chars,
+#With threshold = 0.35, avg recall = 0.893518518518518, avg precision = 0.423535415202082
+#After adding rowwise_fuzzy_score (rule about 3-digit strings),
+#With threshold = 0.35, avg recall = 0.831481481481481, avg precision = 0.422987749742136
+#After adding extract_tokens_horizontally and extract_tokens_vertically instead of n-grams,
+#With threshold = 0.35, avg recall = 0.742592592592592, avg precision = 0.537037037037037
+#After we took off the change about 3-digit strings in rowwise_fuzzy_score, 
+#With threshold = 0.35, avg recall = 0.80462962962963, avg precision = 0.525295075295075
+#With threshold = 0.4, avg recall = 0.747222222222222, avg precision = 0.560678210678211
 
 
 
