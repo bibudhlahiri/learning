5ab48fb85c768331a44a48e0433e444589d43acb
Bibudh Lahiri <blahiri@impetus.co.in>
2017-01-17 11:28:09 +0530

    Removed manual scaling of columns

--- process_kdd.R
+++ process_kdd.R
@@ -21,10 +21,6 @@ load_kdd_data <- function()
   column_names <- c(column_names, "connection_label")
   setnames(data_corr, names(data_corr), column_names)
   
-  #data_corr[, src_bytes := (data_corr$src_bytes - mean(data_corr$src_bytes))/sd(data_corr$src_bytes)]
-  #Checked: mean(data_corr$src_bytes) = -1.852102e-17 and sd(data_corr$src_bytes) = 1
-  #data_corr[, dst_bytes := (data_corr$dst_bytes - mean(data_corr$dst_bytes))/sd(data_corr$dst_bytes)]
-  #Checked: mean(data_corr$dst_bytes) = -1.864818e-17 and sd(data_corr$dst_bytes) = 1
   sc <- c("src_bytes", "dst_bytes", "count", "srv_count", "dst_host_count", "dst_host_srv_count") 
   data_corr <- copy(data_corr)[ , (sc) := lapply(.SD, scale), .SDcols = sc]
   #standardized_filename <- "C:\\Users\\blahiri\\kdd_cup_for_SAx\\kddcup.data.standardized"
