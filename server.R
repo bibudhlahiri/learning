75de5fdefd86e3b2c9ce90ae0a2521f426fc06b6
Bibudh Lahiri <blahiri@impetus.co.in>
2017-02-02 13:42:24 +0530

    First version of running app

--- server.R
+++ server.R
@@ -1,12 +1,94 @@
 library(shiny)
 
-# Define server logic required to draw a histogram
+rf_from_file <<- NA
+file_path <- "C:\\Users\\blahiri\\Toyota\\Paint_Shop_Optimization\\code"
+load(paste(file_path, "\\rf.fitN.rda", sep = ""))
+rf_from_file <<- rf.fitN
+
+custom_fitness <- function(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10)
+{
+  newdata <- data.frame(BP2_1_Robot_Air_Motor = x1, CBTH_Air_House_1_Humidity = x2,
+                        CBTH_Air_House_2_Humidity = x3, CBTH_Air_House_3_Humidity = x4,
+						CBTH_Air_House_5_Humidity = x5, CBTH_Air_House_2_Temp = x6,
+						CBTH_Air_House_5_Temp = x7, CBTH_Air_House_4_Humidity = x8,
+						CP1_4_Robot_Air_Motor = x9, CP2_4_Robot_Air_Motor = x10)
+  #Note: ga() maximizes the fitness function but we want to minimize DPV, hence return negative
+  library(randomForest)
+  raw_prediction <- predict(rf_from_file, newdata)
+  -raw_prediction
+}
+
+optimize_ap_params <- function(input_ranges)
+{
+  library(GA)
+  filename <- "C:\\Users\\blahiri\\Toyota\\Paint_Shop_Optimization\\data\\Phase2\\ICSDefectDataC\\rfImputed.csv"
+  input_data <- read.csv(filename, header = T, stringsAsFactors = F)
+  features <- colnames(input_data)
+  features <- features[(features != "dpv")]
+  
+  #min_values <- as.numeric(sapply(features, function(feature) min(input_data[, feature])))
+  #max_values <- as.numeric(sapply(features, function(feature) max(input_data[, feature])))
+  min_values <- c(input_ranges[["BP2_1_Robot_AM_min"]], input_ranges[["AH1_min_hum"]], input_ranges[["AH2_min_hum"]], 
+                  input_ranges[["AH3_min_hum"]], input_ranges[["AH5_min_hum"]], input_ranges[["AH2_min_temp"]],
+				  input_ranges[["AH5_min_temp"]], input_ranges[["AH4_min_hum"]], input_ranges[["CP1_4_Robot_AM_min"]], 
+				  input_ranges[["CP2_4_Robot_AM_min"]])
+  max_values <- c(input_ranges[["BP2_1_Robot_AM_max"]], input_ranges[["AH1_max_hum"]], input_ranges[["AH2_max_hum"]], 
+                  input_ranges[["AH3_max_hum"]], input_ranges[["AH5_max_hum"]], input_ranges[["AH2_max_temp"]],
+				  input_ranges[["AH5_max_temp"]], input_ranges[["AH4_max_hum"]], input_ranges[["CP1_4_Robot_AM_max"]], 
+				  input_ranges[["CP2_4_Robot_AM_max"]])
+  print(min_values)
+  print(max_values)
+  
+  classes <- sapply(features, function(feature) class(input_data[, feature]))
+  print(classes)
+  
+  GA <- ga(type = "real-valued", 
+           fitness = function(x) custom_fitness(x[1], x[2], x[3], x[4], 
+		                                        x[5], x[6], x[7], x[8], 
+												x[9], x[10]), 
+           min = min_values, max = max_values, maxiter = 5,
+           optim = TRUE)
+  print(summary(GA))
+  paste("Optimal solution is ", paste(paste(features, round(as.numeric(GA@solution), 3), sep = " = "), collapse = ", "), sep = "")
+}
+
 shinyServer(function(input, output) {
 
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-})
+rangeInput <- reactive({
+
+    AH1_min_hum <- input$AH1_min_hum
+	AH1_max_hum <- input$AH1_max_hum
+	BP2_1_Robot_AM_min <- input$BP2_1_Robot_AM_min
+	BP2_1_Robot_AM_max <- input$BP2_1_Robot_AM_max
+	AH2_min_hum <- input$AH2_min_hum
+	AH2_max_hum <- input$AH2_max_hum
+	AH2_min_temp <- input$AH2_min_temp
+	AH2_max_temp <- input$AH2_max_temp
+	CP1_4_Robot_AM_min <- input$CP1_4_Robot_AM_min
+    CP1_4_Robot_AM_max <- input$CP1_4_Robot_AM_max
+	AH3_min_hum <- input$AH3_min_hum
+	AH3_max_hum <- input$AH3_max_hum
+	CP2_4_Robot_AM_min <- input$CP2_4_Robot_AM_min
+	CP2_4_Robot_AM_max <- input$CP2_4_Robot_AM_max
+    AH4_min_hum <- input$AH4_min_hum
+	AH4_max_hum <- input$AH4_max_hum
+	AH5_min_hum <- input$AH5_min_hum
+	AH5_max_hum <- input$AH5_max_hum
+	AH5_min_temp <- input$AH5_min_temp
+	AH5_max_temp <- input$AH5_max_temp
+    
+    list("AH1_min_hum" = AH1_min_hum, "AH1_max_hum" = AH1_max_hum, "BP2_1_Robot_AM_min" = BP2_1_Robot_AM_min,
+	     "BP2_1_Robot_AM_max" = BP2_1_Robot_AM_max, "AH2_min_hum" = AH2_min_hum, "AH2_max_hum" = AH2_max_hum,
+		 "AH2_min_temp" = AH2_min_temp, "AH2_max_temp" = AH2_max_temp, "CP1_4_Robot_AM_min" = CP1_4_Robot_AM_min,
+		 "CP1_4_Robot_AM_max" = CP1_4_Robot_AM_max, "AH3_min_hum" = AH3_min_hum, "AH3_max_hum" = AH3_max_hum,
+		 "CP2_4_Robot_AM_min" = CP2_4_Robot_AM_min, "CP2_4_Robot_AM_max" = CP2_4_Robot_AM_max, 
+		 "AH4_min_hum" = AH4_min_hum, "AH4_max_hum" = AH4_max_hum, "AH5_min_hum" = AH5_min_hum,
+		 "AH5_max_hum" = AH5_max_hum, "AH5_min_temp" = AH5_min_temp, "AH5_max_temp" = AH5_max_temp) 
+  })
+  
+  output$optimal_soln <- renderText({ 
+      input_ranges <- rangeInput()
+      optimize_ap_params(input_ranges)          
+     })
+ }
+)
--- ui.R
+++ ui.R
@@ -1,13 +1,7 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Toyota Fatal Paint Defect Optimizer"),
-
-  # Sidebar with a slider input for the number of bins
-  
+  titlePanel("Toyota Fatal Paint Defect Optimizer"),  
       fluidRow(
 	   column(2, h3("")),
        column(4, h3("Humidity")),
@@ -18,11 +12,10 @@ shinyUI(fluidPage(
 	   column(2, helpText("CBTH Air House 1")),
 	   column(2, numericInput("AH1_min_hum", label = "Min Humidity", value = 55)),
 	   column(2, numericInput("AH1_max_hum", label = "Max Humidity", value = 75)),
-       column(1, numericInput("AH1_min_temp", label = "Min Temp", value = 55)),
-	   column(1, numericInput("AH1_max_temp", label = "Max Temp", value = 75)),
+       column(2, helpText("")),
 	   column(1, helpText("BP2 Robot 1")),
-	   column(1, numericInput("BP2_1_Robot_AM_min", label = "Min Motor Speed", value = 0)),
-	   column(1, numericInput("BP2_1_Robot_AM_max", label = "Max Motor Speed", value = 30000))
+	   column(1, numericInput("BP2_1_Robot_AM_min", label = "Min Speed", value = 0)),
+	   column(1, numericInput("BP2_1_Robot_AM_max", label = "Max Speed", value = 30000))
        ),
 	  fluidRow(
 	   column(2, helpText("CBTH Air House 2")),
@@ -30,12 +23,42 @@ shinyUI(fluidPage(
 	   column(2, numericInput("AH2_max_hum", label = "Max Humidity", value = 75)),
        column(1, numericInput("AH2_min_temp", label = "Min Temp", value = 55)),
 	   column(1, numericInput("AH2_max_temp", label = "Max Temp", value = 75)),
-	   column(1, helpText("BP2 Robot 2")),
-	   column(1, numericInput("BP2_2_Robot_AM_min", label = "Min Motor Speed", value = 0)),
-	   column(1, numericInput("BP2_2_Robot_AM_max", label = "Max Motor Speed", value = 30000))
-       )
-
-    # Show a plot of the generated distribution
-    
+	   column(1, helpText("CP1 Robot 4")),
+	   column(1, numericInput("CP1_4_Robot_AM_min", label = "Min Speed", value = 0)),
+	   column(1, numericInput("CP1_4_Robot_AM_max", label = "Max Speed", value = 30000))
+       ),
+	  fluidRow(
+	   column(2, helpText("CBTH Air House 3")),
+	   column(2, numericInput("AH3_min_hum", label = "Min Humidity", value = 55)),
+	   column(2, numericInput("AH3_max_hum", label = "Max Humidity", value = 75)),
+       column(2, helpText("")),
+	   column(1, helpText("CP2 Robot 4")),
+	   column(1, numericInput("CP2_4_Robot_AM_min", label = "Min Speed", value = 0)),
+	   column(1, numericInput("CP2_4_Robot_AM_max", label = "Max Speed", value = 30000))
+       ),
+	  fluidRow(
+	   column(2, helpText("CBTH Air House 4")),
+	   column(2, numericInput("AH4_min_hum", label = "Min Humidity", value = 55)),
+	   column(2, numericInput("AH4_max_hum", label = "Max Humidity", value = 75)),
+       column(5, helpText(""))
+       ),
+	  fluidRow(
+	   column(2, helpText("CBTH Air House 5")),
+	   column(2, numericInput("AH5_min_hum", label = "Min Humidity", value = 55)),
+	   column(2, numericInput("AH5_max_hum", label = "Max Humidity", value = 75)),
+       column(1, numericInput("AH5_min_temp", label = "Min Temp", value = 55)),
+	   column(1, numericInput("AH5_max_temp", label = "Max Temp", value = 75)),
+	   column(3, helpText(""))
+       ),
+	   fluidRow(  
+         submitButton("What's optimal?")
+       ),
+	   mainPanel(
+         textOutput("optimal_soln")
+    )    
   )
 )
+
+#setwd("C:\\Users\\blahiri\\Toyota\\Paint_Shop_Optimization\\code")
+#library(shiny)
+#runApp("toyota_optimizer")
