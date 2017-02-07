c21737977ee8ffaf0c7aaf4e8e9610bb8ab258e7
Bibudh Lahiri <blahiri@impetus.co.in>
2017-02-07 14:24:16 +0530

    Added method variable_analysis_per_car_process_variable

--- ics_fatal_pf_defects_vs_activeplant.R
+++ ics_fatal_pf_defects_vs_activeplant.R
@@ -6,7 +6,6 @@ library(rpart)
 library(party)
 
 dtree <- NA
-rf.fitN <- NA
 
 load_ics_activeplant_data <- function()
 {
@@ -52,7 +51,11 @@ build_rf <- function(GIAPJ_AllM_New)
   library(randomForest)
   #To handle Missing Value in data, using imputation from "X", "RecordTime", "tdefect", "tveh",
   rfImputed <- rfImpute(dpv~. , GIAPJ_AllM_New[,-c(1:4)])
-  rf.fitN <<- randomForest(dpv~. , rfImputed)
+  rf.fitN <- randomForest(dpv~. , rfImputed)
+  file_path <- "C:\\Users\\blahiri\\Toyota\\Paint_Shop_Optimization\\code"
+  save(rf.fitN, file = paste(file_path, "\\rf.fitN.rda", sep = ""))
+  filename <- "C:\\Users\\blahiri\\Toyota\\Paint_Shop_Optimization\\data\\Phase2\\ICSDefectDataC\\rfImputed.csv"
+  write.table(rfImputed, file = filename, sep = ",", row.names = FALSE, col.names = TRUE)
   rfImputed
 }
 
@@ -64,30 +67,33 @@ custom_fitness <- function(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10)
 						CBTH_Air_House_5_Temp = x7, CBTH_Air_House_4_Humidity = x8,
 						CP1_4_Robot_Air_Motor = x9, CP2_4_Robot_Air_Motor = x10)
   #Note: ga() maximizes the fitness function but we want to minimize DPV, hence return negative
-  #raw_prediction <- predict(dtree, newdata, type = "response")
-  raw_prediction <- predict(rf.fitN, newdata)
+  library(randomForest)
+  raw_prediction <- predict(rf_from_file, newdata)
   #cat(paste("x1 = ", x1, ", x2 = ", x2, ", raw_prediction = ", raw_prediction, "\n", sep = ""))
   -raw_prediction
 }
 
-optimize_ap_params <- function(input_data)
+optimize_ap_params <- function()
 {
   library(GA)
+  filename <- "C:\\Users\\blahiri\\Toyota\\Paint_Shop_Optimization\\data\\Phase2\\ICSDefectDataC\\rfImputed.csv"
+  input_data <- read.csv(filename, header = T, stringsAsFactors = F)
   features <- colnames(input_data)
-  features <- features[!(features %in% c("X", "RecordTime", "tdefect", "tveh", "dpv"))]
-  print(features)
+  features <- features[(features != "dpv")]
+
   min_values <- as.numeric(sapply(features, function(feature) min(input_data[, feature])))
   max_values <- as.numeric(sapply(features, function(feature) max(input_data[, feature])))
   print(min_values)
   print(max_values)
-    
+  
   GA <- ga(type = "real-valued", 
            fitness = function(x) custom_fitness(x[1], x[2], x[3], x[4], 
 		                                        x[5], x[6], x[7], x[8], 
 												x[9], x[10]), 
-           min = min_values, max = max_values,
-           optim = TRUE)
+           min = min_values, max = max_values, maxiter = 5,
+           optim = TRUE, keepBest = TRUE)
   print(summary(GA))
+  cat(paste("Optimal solution is ", paste(paste(features, as.numeric(GA@solution), sep = " = "), collapse = ", "), sep = ""))
   GA
 }
 
@@ -127,6 +133,27 @@ jump_of_air_motor_within_an_hour <- function()
   BP1_1_Robot_AM
 }
 
+#Analyze the variance per variables per process per car and take means, SD and CV.
+#Do not analyze by hour. 
+variable_analysis_per_car_process_variable <- function()
+{
+  filename <- "C:\\Users\\blahiri\\Toyota\\Paint_Shop_Optimization\\data\\Phase2\\ICSDefectDataC\\avg_date_galc_join_activePlant_SelectedVins\\avg_date_galc_join_activePlant_SelectedVins.csv"
+  selectedVINs <- read.csv(filename, header = T, stringsAsFactors = F)
+  selectedVINs <- selectedVINs %>% filter(substr(variablename, nchar(variablename) - 4, nchar(variablename)) != "Color")
+  variables_by_vehicles <- selectedVINs %>% select(vin, eventname, variablename, value) %>% group_by(vin, eventname, variablename) %>% summarise(average = mean(value), std_dev = sd(value))
+  variables_by_vehicles$coeff_of_var <- ifelse((variables_by_vehicles$average == 0), 0, 
+                                               variables_by_vehicles$std_dev/variables_by_vehicles$average)
+  filename <- "C:\\Users\\blahiri\\Toyota\\Paint_Shop_Optimization\\data\\Phase2\\ICSDefectDataC\\avg_date_galc_join_activePlant_SelectedVins\\variables_by_vehicles.csv"
+  write.table(variables_by_vehicles, file = filename, sep = ",", row.names = FALSE, col.names = TRUE)
+  
+  #Take the median CVs per process and variable now
+  median_CV_by_var <- variables_by_vehicles %>% ungroup() %>% select(eventname, variablename, coeff_of_var) %>% group_by(eventname, variablename) %>% summarise(median_CV = median(coeff_of_var))
+  filename <- "C:\\Users\\blahiri\\Toyota\\Paint_Shop_Optimization\\data\\Phase2\\ICSDefectDataC\\avg_date_galc_join_activePlant_SelectedVins\\median_CV_by_var.csv"
+  write.table(median_CV_by_var, file = filename, sep = ",", row.names = FALSE, col.names = TRUE)
+  median_CV_by_var
+}
+
+
 #source("C:\\Users\\blahiri\\Toyota\\Paint_Shop_Optimization\\code\\ics_fatal_pf_defects_vs_activeplant.R")
 #ics_activeplant <- load_ics_activeplant_data()
 #dpv_by_hour <- compute_dpvs(ics_activeplant)
@@ -134,9 +161,12 @@ jump_of_air_motor_within_an_hour <- function()
 #GA <- optimize_ap_params(dpv_by_hour)
 #check_hourly_CVs()
 #BP1_1_Robot_AM <- jump_of_air_motor_within_an_hour()
-
 #GIAPJ_AllM_New <- load_activeplant_hourly_avg_data()
-rfImputed <- build_rf(GIAPJ_AllM_New)
-GA <- optimize_ap_params(rfImputed)
-
+#rfImputed <- build_rf(GIAPJ_AllM_New)
+#rf_from_file <<- NA
+#file_path <- "C:\\Users\\blahiri\\Toyota\\Paint_Shop_Optimization\\code"
+#load(paste(file_path, "\\rf.fitN.rda", sep = ""))
+#rf_from_file <<- rf.fitN
+#GA <- optimize_ap_params()
+median_CV_by_var <- variable_analysis_per_car_process_variable() 
 
